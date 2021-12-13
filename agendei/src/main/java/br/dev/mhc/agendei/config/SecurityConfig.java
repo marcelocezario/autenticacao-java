cdc448c32228f83da30abc435f1a3571cbfb628d
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-12-13 14:14:39 -0300

    Liberação end-points por tipo de chamada

--- agendei/src/main/java/br/dev/mhc/agendei/config/SecurityConfig.java
+++ agendei/src/main/java/br/dev/mhc/agendei/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,27 +18,33 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	/**
 	 * Configuração para h2 database
 	 */
 	@Autowired
 	private Environment env;
 
-	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**", "/users" };
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**", "/test/**" };
+
+	private static final String[] PUBLIC_MATCHERS_GET = {};
+
+	private static final String[] PUBLIC_MATCHERS_POST = { "/users/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		/**
 		 * Configuração para h2 database
 		 */
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
+
 		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll().anyRequest().authenticated();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
